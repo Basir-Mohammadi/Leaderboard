3bd75f22be555b6acd0356da19cda134fbf28882
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-22 17:06:59 +0100

    Create the fetch API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // import _ from 'lodash';
 import './style.css';
+
+fetch(
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7rmDPOHHJfcKgrMrYAGZ/scores/',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'My cool new game',
+      user: 'John Doe',
+      score: 100,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  },
+)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7rmDPOHHJfcKgrMrYAGZ/scores/')
+  .then(response => response.json())
+  .then(json => console.log(json.result));
