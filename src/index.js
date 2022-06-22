2abf773e89c520283ba38a626894e97dc39ce6e4
Basir Ahmad Mohammadi <basirahmadmohammadi852@gmail.com>
2022-06-22 17:59:44 +0100

    Add addscore and refreshscore functions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 // import _ from 'lodash';
 import './style.css';
 
-fetch(
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7rmDPOHHJfcKgrMrYAGZ/scores/',
-  {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'My cool new game',
-      user: 'John Doe',
-      score: 100,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  },
-)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+import { addScore, refreshScores } from './score.js';
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7rmDPOHHJfcKgrMrYAGZ/scores/')
-  .then(response => response.json())
-  .then(json => console.log(json.result));
+const form = document.querySelector('.text-input-score');
+const refresh = document.querySelector('.refresh-btn');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addScore(e);
+  form.reset();
+});
+
+refresh.addEventListener('click', () => {
+  refreshScores();
+});
---
+++ src/score.js
@@ -0,0 +1,34 @@
+export const refreshScores = async () => {
+  const scoresContainer = document.querySelector('.recent-score-body');
+  scoresContainer.innerHTML = '';
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
+  );
+  const json = await response.json();
+
+  json.result.forEach((score) => {
+    scoresContainer.innerHTML += `
+    <div><span>${score.user} :</span><span>${score.score}</span></div>
+    `;
+  });
+};
+
+export const addScore = async (e) => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U89UpEn5EXu1MQahYhm3/scores/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'My cool new game',
+        user: `${e.target.name.value}`,
+        score: `${e.target.score.value}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  );
+  return response;
+};
+
+window.addEventListener('load', refreshScores);
